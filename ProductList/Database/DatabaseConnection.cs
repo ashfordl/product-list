183e77386d31febfd93f1fbba38b546903b967a7
ashfordl <luke.a.ashford@gmail.com>
2014-10-27 18:33:55 +0000

    Create database util class

---
+++ ProductList/Database/DatabaseConnection.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite.Net;
+using SQLite.Net.Interop;
+using SQLite.Net.Platform.Generic;
+using ProductList.Models;
+
+namespace ProductList.Database
+{
+    public class DatabaseConnection
+    {
+        public static SQLiteConnection CreateDatabase(string path)
+        {
+            SQLiteConnection db = new SQLiteConnection(new SQLitePlatformGeneric(), path);
+            db.CreateTable<Category>();
+            db.CreateTable<Product>();
+
+            return db;
+        }
+    }
+}
