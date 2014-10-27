3c07cc4fe507023f217a088e6a6cf578b7b3b55f
ashfordl <luke.a.ashford@gmail.com>
2014-10-27 19:00:11 +0000

    Add basic menu functions

--- ProductList/MainWindow.xaml.cs
+++ ProductList/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ProductList.Database;
 
 namespace ProductList
 {
@@ -23,6 +25,65 @@ namespace ProductList
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void NewCommand_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void NewCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            SaveFileDialog dialog = new SaveFileDialog();
+            dialog.Title = "New Product List";
+            dialog.FileName = "List1.db";
+            dialog.Filter = "Database File (*.db)|*.db|All Files(*.*)|*.*";
+
+            if (dialog.ShowDialog() == true)
+            {
+                try
+                {
+                    // DatabaseOperations.CreateDatabase(dialog.FileName);
+                    DatabaseConnection.CreateDatabase(dialog.FileName);
+                }
+                catch (Exception ex) // In case anything went wrong
+                {
+                    MessageBox.Show("An error occurred and the list was not created.\n\n" + ex.ToString(),
+                                    "Error",
+                                    MessageBoxButton.OK,
+                                    MessageBoxImage.Error);
+                }
+
+                // TODO Load file
+            }
+        }
+
+        private void OpenCommand_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void OpenCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Title = "New Product List";
+            dialog.FileName = "List1.db";
+            dialog.Filter = "Database File (*.db)|*.db|All Files(*.*)|*.*";
+
+            if (dialog.ShowDialog() == true)
+            {
+                try
+                {
+                    // TODO Load file
+                }
+                catch (Exception ex) // In case anything went wrong
+                {
+                    MessageBox.Show("An error occurred and the list was not created.",
+                                    "Error",
+                                    MessageBoxButton.OK,
+                                    MessageBoxImage.Error);
+                }
+            }
         }
     }
 }
